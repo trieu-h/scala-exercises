14572a3805591b2b4b844780160bf2b0781020b0
Trieu <lehuynhhaitrieu1996@gmail.com>
2022-01-04 22:00:11 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Scala exercise
+My attempt at solving the exercises listed in the book [Functional Programming in Scala](https://www.manning.com/books/functional-programming-in-scala)
---
+++ exercise.scala
@@ -0,0 +1,30 @@
+object Excersise {
+  def fib(n: Int): Int = {
+    assert(n > 0)
+    @annotation.tailrec
+    def fib_(n: Int, sum: Int, prev: Int): Int = {
+      if (n > 0) fib_(n - 1, sum + prev, sum)
+      else sum
+    }
+
+    fib_(n - 1, 0, 1)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    val length =
+      (as zip as.drop(1))
+      .map{ p =>
+        val (n1, n2) = p
+        ordered(n1, n2)
+      }
+      .filter(_ == false)
+      .length
+
+    !(length > 0)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(fib(2))
+    println(isSorted(Array(2,1,3), (x: Int, y: Int) => x - y < 0))
+  }
+}
