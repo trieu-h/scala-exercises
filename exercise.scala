d65f9772f632c84ced2cca81d4637bf3b6283fac
Trieu <lehuynhhaitrieu1996@gmail.com>
2022-01-05 15:52:16 +0700

    Implement uncurry and compose

--- exercise.scala
+++ exercise.scala
@@ -23,6 +23,15 @@ object Excersise {
     !(length > 0)
   }
 
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
+    (a: A) => (b: B) => f(a, b)
+
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b)
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C =
+    (a: A) => f(g(a))
+
   def main(args: Array[String]): Unit = {
     println(fib(2))
     println(isSorted(Array(2,1,3), (x: Int, y: Int) => x - y < 0))
